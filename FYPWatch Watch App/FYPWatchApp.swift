ed3c92a32da33535c9d1c68fe2a029ab4e107c0c
Lee Chilvers <lee.chilvers@icloud.com>
2024-01-07 17:51:56 +0000

    Initial commit

---
+++ FYPWatch Watch App/FYPWatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  FYPWatchApp.swift
+//  FYPWatch Watch App
+//
+//  Created by Lee Chilvers on 07/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct FYPWatch_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
