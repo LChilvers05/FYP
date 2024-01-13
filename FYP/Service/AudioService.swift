13ce96296683f587b938a600e8f14c6ee4e3976b
Lee Chilvers <lee.chilvers@icloud.com>
2024-01-13 17:13:50 +0000

    create AudioService

---
+++ FYP/Service/AudioService.swift
@@ -0,0 +1,16 @@
+//
+//  AudioService.swift
+//  FYP
+//
+//  Created by Lee Chilvers on 13/01/2024.
+//
+
+import AudioKit
+
+final class AudioService {
+    
+    static let shared = AudioService()
+    private init() {
+        // TODO: find v5 documentation
+    }
+}
